f8c3ef26bced6ebce958385619fbb60996698b91
Programming Web <65061300+farozy@users.noreply.github.com>
2022-11-04 12:54:33 +0700

    Update footer.php

--- app/Views/templates/footer.php
+++ app/Views/templates/footer.php
@@ -13,44 +13,7 @@
 <script>
     (() => {
         $('[data-toggle="tooltip"]').tooltip();
-        var i = "theme",
-            o = /\btheme-[a-z0-9]+\b/g,
-            r = document.getElementById("toggle-dark");
-
-
-        function l(e) {
-            var t = arguments.length > 1 && void 0 !== arguments[1] && arguments[1];
-            (document.body.className = document.body.className.replace(o, "")),
-            document.body.classList.add(e),
-                (r.checked = "light-mode" == e),
-                t || localStorage.setItem(i, e);
-        }
-        r.addEventListener("input", function(e) {
-                if (e.target.checked) {
-                    l("light-mode");
-                    $('body').removeClass('dark-mode')
-                } else {
-                    l("dark-mode");
-                    $('body').removeClass('light-mode')
-                }
-            }),
-            document.addEventListener("DOMContentLoaded", function() {
-                var e;
-                if ((e = localStorage.getItem(i))) return l(e);
-                if (window.matchMedia) {
-                    var t = window.matchMedia("(prefers-color-scheme: light)");
-                    return (
-                        t.addEventListener("change", function(e) {
-                            return l(e.matches ? "light-mode" : "dark-mode", !0);
-                        }),
-                        l(t.matches ? "light-mode" : "dark-mode", !0)
-                    );
-                }
-            });
-
-
-
-
+       
         $('#logout').click(function(e) {
             e.preventDefault();
 
